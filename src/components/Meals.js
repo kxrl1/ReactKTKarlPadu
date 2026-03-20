e0c08cb43628a67c11304c2bde3fde6917c1c673
kxrl1 <karl.padu@voco.ee>
2026-03-20 11:54:18 +0200

    Ülesanne 3

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react";  
+
 const Meals = () => {
+    useEffect(() => {
+        const dataMeals = async () => {
+            try {
+                const response = await fetch('http://localhost:3001/meals')
+                const mealsData = await response.json()
+                console.log(mealsData)
+            } catch (error) {
+                console.error('Viga', error)
+            }
+        }
+        dataMeals()
+    }, []);
     return (
         <ul id="meals">
-            { 
-                <h2>Meals</h2>
+            {
+               <h2>Meals</h2> 
             }
         </ul>
     )
