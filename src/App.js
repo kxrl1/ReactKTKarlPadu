1bb7f9e1db65767f5735fe4f1d5563eae991e506
kxrl1 <karl.padu@voco.ee>
2026-03-20 15:49:43 +0200

    Ülesanne 8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 import CartContext from "./store/CartContext";
+import { useReducer } from "react";
+
+function cartReducer(state, action) {
+  if (action.type === 'ADD_ITEM') {
+    const existingIndex = state.items.findIndex(i => i.id === action.item.id);
+    const updatedItems = [...state.items];
+    if (existingIndex >= 0) {
+      updatedItems[existingIndex] = {
+        ...updatedItems[existingIndex],
+        quantity: updatedItems[existingIndex].quantity + 1,
+      };
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+    return { ...state, items: updatedItems };
+  }
+  return state;
+}
 
 const App = () => {
- return (
-    <CartContext.Provider value={{ items: [], addItem: () => {} }}>
+  const [cart, dispatch] = useReducer(cartReducer, { items: [] });
+
+  function addItem(item) {
+    dispatch({ type: 'ADD_ITEM', item });
+  }
+
+  return (
+    <CartContext.Provider value={{ items: cart.items, addItem }}>
       <Header />
       <Meals />
     </CartContext.Provider>
