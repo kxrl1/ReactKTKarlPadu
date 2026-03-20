c02d2dd837d5515d5bb589b9630a557aceb305a6
kxrl1 <karl.padu@voco.ee>
2026-03-20 13:45:17 +0200

    Ülesanne 5

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -5,7 +5,7 @@ const MealItem = (props) => {
                 <img src={`http://localhost:3001/${props.image}`} alt={props.name} />
                 <div>
                     <h3>{props.name}</h3>
-                    <p className="meal-item-price">{props.price}</p>
+                    <p className="meal-item-price">{new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(props.price)}</p>
                     <p className="meal-item-description">{props.description}</p>
                 </div>
                 <p className="meal-item-actions">
